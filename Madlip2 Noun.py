78c16f18d4c617f50be2dd91cc0eb771c2432ef6
lwbinary <lwbinary@gmail.com>
2021-12-03 22:16:29 -0800

    pr demo (#3)

--- Madlip2 Noun.py
+++ Madlip2 Noun.py
@@ -5,8 +5,7 @@ input = input("please type a noun:")
 def isNoun(word):
     if input in nouns: 
         return True
-    else:
-        return False
+    return False
         
 result = isNoun(word)
 print(f"result:{result}")
