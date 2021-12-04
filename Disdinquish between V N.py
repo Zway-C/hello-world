22eba2f885d2d7745441940b841a23a7394fc745
ZhiWei Chen <Zhiwei9982@gmail.com>
2021-12-03 20:29:15 -0800

    add 1st python file

---
+++ Disdinquish between V N.py
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+noun = ('cat', 'house', 'table')
+verb = ('kick', 'jump', 'sleep')
+
+word = input("please type a word: ")
+
+
+
+while word in noun:
+    print ("noun")
+
+while word in verb:
+    print ("verb")
+
+def distinquish():
+    if input not in noun or verb:
+        return ("please type a new word")
+
+print(distinquish())
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
